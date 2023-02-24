3ec68a41750ef4b155b3ce935fd6da4624a796c7
senura98 <senuraniasl1998@gmail.com>
2023-02-24 18:14:14 +0530

    Finalized the back end work

--- server/models/User.js
+++ server/models/User.js
@@ -1,45 +1,45 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        min:2,
-        max:50
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
     },
-    lastName:{
-        type:String,
-        required:true,
-        min:2,
-        max:50
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
     },
-    email:{
-        type:String,
-        required:true,
-        max:50,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
     },
-    password:{
-        type:String,
-        require:true,
-        min:5,   
+    password: {
+      type: String,
+      required: true,
+      min: 5,
     },
-    picturePath:{
-        type:String,
-        default:''
+    picturePath: {
+      type: String,
+      default: "",
     },
-    friends:{
-        type:Array,
-        default:[]
+    friends: {
+      type: Array,
+      default: [],
     },
-    location:String,
-    occupation:String,
-    viewedProfile:Number,
-    impressions:Number
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
 
-
-},{timestamps:true}
-)
-
-const User=mongoose.model("User",UserSchema);
+const User = mongoose.model("User", UserSchema);
 export default User;
